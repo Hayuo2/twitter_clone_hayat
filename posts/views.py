8848f64670a6268d332122176014b7a3fe0fad85
Hayuo2 <94030754+Hayuo2@users.noreply.github.com>
2022-04-15 21:17:12 -0400

    updated the views.py file

--- posts/views.py
+++ posts/views.py
@@ -31,9 +31,7 @@ def delete(request, post_id):
     return HttpResponseRedirect("/")
 
 def edit(request, post_id):
-    # Find post
-    if request.method == "GET":
-        post = Post.objects.get(id=post_id)
+    post = Post.objects.get(id=post_id)
     if request.method == 'POST':
         editpost = Post.objects.get(id=post_id)
         form = PostForm(request.POST, request.FILES, instance=post)
